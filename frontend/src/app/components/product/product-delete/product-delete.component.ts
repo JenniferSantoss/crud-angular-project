6cfef23ad312e7266e3632e095da79a62285ae24
Jennifer Santos <jennylima10@gmail.com>
2023-07-10 22:11:03 -0300

    Correção do Erro no delete product

--- frontend/src/app/components/product/product-delete/product-delete.component.ts
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -25,7 +25,10 @@ export class ProductDeleteComponent implements OnInit {
   }
 
   deleteProduct(): void {
-
+    this.productService.delete(this.product.id).subscribe(() => {
+      this.productService.showMessage("Produto excluido com sucesso!");
+      this.router.navigate(["/products"]);
+    });
   }
 
   cancel(): void {
